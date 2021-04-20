3520f8893f309ab29d6e8d77eb9431721d655ff8
isna prasetyo <praseetyaa@gmail.com>
2021-04-20 15:42:35 +0700

    admin style

--- resources/views/template/admin/_css.blade.php
+++ resources/views/template/admin/_css.blade.php
@@ -1,10 +1,10 @@
 <style type="text/css">
     #navbarSupportedContent {background: {{ get_warna_primer() }}!important;}
-    .navbar-header[data-logobg=skin5] {background: {{ get_warna_sekunder() }}!important;}
+    .navbar-header[data-logobg=skin5] {background: var(--white)!important;}
     #main-wrapper .left-sidebar[data-sidebarbg=skin5], #main-wrapper .left-sidebar[data-sidebarbg=skin5] ul {background: {{ get_warna_tersier() }}!important;}
     .page-wrapper {background: linear-gradient(180deg, #eeeeee, #ffffff)!important;}
     .page-wrapper > .container-fluid {min-height: calc(100vh - 163px);}
-    .logo-text {font-weight: bold; font-size: 1.3rem; text-transform: uppercase; color: {{ get_warna_teks_logo() }}!important;}
+    .logo-text {font-weight: bold; font-size: 1.3rem; text-transform: uppercase; color: var(--color-1)!important;}
     .navbar-nav .dropdown-menu {background-color: #eeeeee;}
     .dropdown-menu .dropdown-item:hover {background-color: #dddddd;}
     .sidebar-nav ul .sidebar-item .sidebar-link {padding: 8px;}
@@ -25,6 +25,7 @@
         --border-light: rgba(2555,255,255,.5); 
         --shadow: 0 .125rem .25rem rgba(0,0,0,.075);
         --transition: .25s ease;
+        --transition-cubic: .5s cubic-bezier(0.65, 0.05, 0.36, 1);
         --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"!important;
     }
     .rounded-1{border-radius: .5em!important}
@@ -53,4 +54,6 @@
     .alert, .card, .navbar-nav .dropdown-menu{border-radius: .5em}
     .navbar-nav .dropdown-menu{background-color: var(--white)}
     .navbar-nav .dropdown-menu .dropdown-item.bg-success{background-color: var(--color-1)!important; color: var(--white)}
+    .topbar .top-navbar .navbar-header{transition: var(--transition); background-color: var(--white)}
+  
 </style>
--- resources/views/template/admin/_header.blade.php
+++ resources/views/template/admin/_header.blade.php
@@ -1,65 +1,29 @@
-
-        <!-- ============================================================== -->
-        <!-- Topbar header - style you can find in pages.scss -->
-        <!-- ============================================================== -->
         <header class="topbar" data-navbarbg="skin5">
             <nav class="navbar top-navbar navbar-expand-md navbar-dark">
                 <div class="navbar-header" data-logobg="skin5">
-                    <!-- This is for the sidebar toggle which is visible on mobile only -->
                     <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
-                    <!-- ============================================================== -->
-                    <!-- Logo -->
-                    <!-- ============================================================== -->
                     <a class="navbar-brand" href="/" target="_blank">
-                        <!-- Logo icon -->
                         <b class="logo-icon p-l-10">
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
                             <img src="{{ asset('assets/images/logo/'.get_icon()) }}" height="30" alt="homepage" class="light-logo" />
                            
                         </b>
-                        <!--End Logo icon -->
-                         <!-- Logo text -->
                         <span class="logo-text ml-2">
-                             <!-- dark Logo text -->
                              Admin Area
-                             <!-- <img src="{{ asset('assets/images/logo/'.get_logo()) }}" height="30" alt="homepage" class="light-logo" /> -->
-                            
                         </span>
-                        <!-- Logo icon -->
-                        <!-- <b class="logo-icon"> -->
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
-                            <!-- <img src="{{ asset('templates/matrix-admin/assets/images/logo-text.png') }}" alt="homepage" class="light-logo" /> -->
-                            
-                        <!-- </b> -->
-                        <!--End Logo icon -->
                     </a>
-                    <!-- ============================================================== -->
-                    <!-- End Logo -->
-                    <!-- ============================================================== -->
-                    <!-- ============================================================== -->
-                    <!-- Toggle which is visible on mobile only -->
-                    <!-- ============================================================== -->
                     <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="ti-more"></i></a>
                 </div>
-                <!-- ============================================================== -->
-                <!-- End Logo -->
-                <!-- ============================================================== -->
                 <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
-                    <!-- ============================================================== -->
-                    <!-- toggle and nav items -->
-                    <!-- ============================================================== -->
                     <ul class="navbar-nav float-left mr-auto">
                         <li class="nav-item d-none d-md-block"><a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)" data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a></li>
                     </ul>
-                    <!-- ============================================================== -->
-                    <!-- Right side toggle and nav items -->
-                    <!-- ============================================================== -->
+                    <div class="input-group">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text bg-transparent border-right-0" id="basic-addon1" style="border-radius: 1.5em 0 0 1.5em"><i class="ti-search font-weight-bold"></i></span>
+                      </div>
+                      <input type="text" class="form-control border-left-0" placeholder="Pencarian" aria-label="Pencarian" aria-describedby="basic-addon1" style="border-radius: 0 1.5em 1.5em 0">
+                    </div>
                     <ul class="navbar-nav float-right">
-                        <!-- ============================================================== -->
-                        <!-- Messages -->
-                        <!-- ============================================================== -->
                         @if(Auth::user()->role == role_it())
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -147,13 +111,6 @@
                                 </ul>
                             </div>
                         </li>
-                        <!-- ============================================================== -->
-                        <!-- End Messages -->
-                        <!-- ============================================================== -->
-
-                        <!-- ============================================================== -->
-                        <!-- User profile and search -->
-                        <!-- ============================================================== -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ Auth::user()->foto != '' ? asset('assets/images/users/'.Auth::user()->foto) : asset('assets/images/default/user.jpg') }}" alt="user" class="rounded-circle" width="31"></a>
                             <div class="dropdown-menu dropdown-menu-right user-dd animated">
@@ -169,13 +126,7 @@
                                 </form>
                             </div>
                         </li>
-                        <!-- ============================================================== -->
-                        <!-- User profile and search -->
-                        <!-- ============================================================== -->
                     </ul>
                 </div>
             </nav>
         </header>
-        <!-- ============================================================== -->
-        <!-- End Topbar header -->
-        <!-- ============================================================== -->
